ff9dce6183e17ae0511b174eb312a6c0696abe02
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-07-06 17:57:32 +0200

    return an object that contains the properties specimenNum and dna that correspond to the parameters provided.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Challenge Project: Mysterious Organism
+## Project Goals
+
+To create objects that simulate the DNA of P. aequor for your research team to study.
+
+
+## Technologies
+
+* 		JavaScript
+* 		Node
+
+
--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 // Returns a random DNA base
 const returnRandBase = () => {
   const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
+  const result =  dnaBases[Math.floor(Math.random() * 4)];
+  return result
+  
 };
+
+returnRandBase()
+// console.log('BASE',returnRandBase())
 
 // Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
@@ -12,9 +17,35 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
+mockUpStrand()
+// console.log('RANDOM DNA',mockUpStrand())
 
+const newStrand = mockUpStrand();
 
+//pAequorFactory() should return an object that contains the properties specimenNum and dna that correspond to the parameters provided.
+const pAequorFactory = (numParam, basesParam , pAequor)=>{
 
+  return {
+    specimenNum: numParam,
+    dna: basesParam,
+    compare: pAequor,
+    mutate() {
+      console.log('OBJECT', this.dna)
+      return 
+    },
+    compareDNA() {
+      console.log('COMPARE', this.compare)
+      return 
+    }
+  }
+
+}
+const newDna = pAequorFactory(1, newStrand);
+
+newDna.mutate('cacaDNA');
+// const newMutation = pAequorFactory(1, newStrand);
+// newMutation.mutate();
+// console.log(newMutation === newStrand[0])
 
 
 
